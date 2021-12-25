bf9debd29cde7e7649acdb0afd60bee683f2de81
xXDevSSHXx <sepehr86.HomeWork@gmail.com>
2021-12-25 13:04:27 +0330

    Added Base Features

--- ProCapture/MainForm.Designer.cs
+++ ProCapture/MainForm.Designer.cs
@@ -4,12 +4,12 @@ namespace ProCapture
     partial class MainForm
     {
         /// <summary>
-        /// Required designer variable.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -24,25 +24,95 @@ namespace ProCapture
         #region Windows Form Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.exitButton = new System.Windows.Forms.PictureBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.scanButton = new System.Windows.Forms.Button();
+            this.FolderTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.exitButton)).BeginInit();
             this.SuspendLayout();
+            // 
+            // exitButton
+            // 
+            this.exitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.exitButton.Image = global::ProCapture.Resources.Close;
+            this.exitButton.Location = new System.Drawing.Point(411, 5);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(25, 25);
+            this.exitButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.exitButton.TabIndex = 0;
+            this.exitButton.TabStop = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Segoe UI", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.statusLabel.Location = new System.Drawing.Point(150, 68);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(140, 59);
+            this.statusLabel.TabIndex = 1;
+            this.statusLabel.Text = "Ready";
+            // 
+            // scanButton
+            // 
+            this.scanButton.AutoEllipsis = true;
+            this.scanButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.scanButton.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.scanButton.Location = new System.Drawing.Point(156, 191);
+            this.scanButton.Name = "scanButton";
+            this.scanButton.Size = new System.Drawing.Size(129, 42);
+            this.scanButton.TabIndex = 2;
+            this.scanButton.Text = "Scan";
+            this.scanButton.UseVisualStyleBackColor = true;
+            this.scanButton.Click += new System.EventHandler(this.scanButton_Click);
+            // 
+            // FolderTextBox
+            // 
+            this.FolderTextBox.Location = new System.Drawing.Point(71, 148);
+            this.FolderTextBox.Name = "FolderTextBox";
+            this.FolderTextBox.PlaceholderText = "Minecraft Folder";
+            this.FolderTextBox.Size = new System.Drawing.Size(332, 23);
+            this.FolderTextBox.TabIndex = 3;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(568, 321);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(33)))), ((int)(((byte)(68)))));
+            this.ClientSize = new System.Drawing.Size(440, 306);
+            this.ControlBox = false;
+            this.Controls.Add(this.FolderTextBox);
+            this.Controls.Add(this.scanButton);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.exitButton);
+            this.ForeColor = System.Drawing.Color.Snow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "MainForm";
-            this.Text = "MainForm";
-            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Tag = "";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.exitButton)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.PictureBox exitButton;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Button scanButton;
+        private System.Windows.Forms.TextBox FolderTextBox;
     }
+
+
 }
--- ProCapture/MainForm.cs
+++ ProCapture/MainForm.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,14 +15,99 @@ namespace ProCapture
 {
     public partial class MainForm : Form
     {
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.Style = 0x20000;
+                return cp;
+            }
+        }
+
         public MainForm()
         {
             InitializeComponent();
         }
 
-        private void MainForm_Load(object sender, EventArgs e)
+        private void Form_MouseDown(object sender, MouseEventArgs e)
         {
+            ReleaseCapture();
+            SendMessage(Handle, 0x112, 0xf012, 0);
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(this, "The Scan Isn`t Complete Yet. Do You Want To Exit?", "ProCapture", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        public string Title => "Pro Capture";
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            Pen pen = new Pen(ThemeInfo.GetThemeColor(), 2);
+            Rectangle rectangle = new Rectangle(0, 0, Width, Height);
+            graphics.DrawRectangle(pen, rectangle);
+            graphics.DrawImage(Icon.ToBitmap(), 5, 5, 24, 24);
+            graphics.DrawString(Title, Font, new SolidBrush(Color.Snow), 30, 11);
+            base.OnPaint(e);
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            FolderTextBox.PlaceholderText = $@"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\.minecraft";
+        }
+        private string mcPath;
+        private async void scanButton_Click(object sender, EventArgs e)
+        {
+            mcPath = string.IsNullOrEmpty(FolderTextBox.Text) ? FolderTextBox.PlaceholderText : FolderTextBox.Text;
+            if (!Directory.Exists(mcPath))
+            {
+                MessageBox.Show("Minecraft Folder Not Found.Enter A Valid Path");
+                return;
+            }
+            await Scan();
+        }
+
+        private async Task Scan()
+        {
+            await Task.Run(() =>
+            {
+                Dictionary<string, Object> jsonData = new Dictionary<string, Object>();
+
+                var versions = Directory.GetDirectories(mcPath + @"\versions").ToList();
+                var mods = Directory.GetFiles(mcPath + @"\mods").ToList();
+
+                var versionNames = new List<string>();
+                var modNames = new List<string>();
+
+                foreach (var item in versions)
+                {
+                    versionNames.Add(new DirectoryInfo(item).Name);
+                }
+                foreach (var item in mods)
+                {
+                    modNames.Add(new FileInfo(item).Name);
+                }
+                foreach (var item in Directory.GetDirectories(mcPath + @"\mods"))
+                {
+                    modNames.Add(new DirectoryInfo(item).Name);
+                }
+                jsonData.Add("versions", versionNames);
+                jsonData.Add("mods", modNames);
+
+                string json = JsonConvert.SerializeObject(jsonData);
+                MessageBox.Show(json);
+            });
 
         }
     }
+
 }

fa0885756cd1eec3d6c65e9a5741c40adf1c97fd
xXDevSSHXx <sepehr86.HomeWork@gmail.com>
2021-12-25 15:57:18 +0330

    Made The App Ready for Test Release

--- ProCapture/MainForm.cs
+++ ProCapture/MainForm.cs
@@ -1,15 +1,17 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+﻿using System;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
 using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+using System.Threading;
 
 namespace ProCapture
 {
@@ -42,10 +44,12 @@ namespace ProCapture
 
         private void exitButton_Click(object sender, EventArgs e)
         {
-            if (MessageBox.Show(this, "The Scan Isn`t Complete Yet. Do You Want To Exit?", "ProCapture", MessageBoxButtons.YesNo) == DialogResult.Yes)
-            {
-                Application.Exit();
-            }
+            if (isWorking)
+                if (MessageBox.Show(this, "The Scan Isn`t Complete Yet. Do You Want To Exit?", "ProCapture", MessageBoxButtons.YesNo) == DialogResult.No)
+                {
+                    return;
+                }
+            Application.Exit();
         }
 
         public string Title => "Pro Capture";
@@ -76,22 +80,33 @@ namespace ProCapture
             await Scan();
         }
 
+        private bool isWorking = false;
         private async Task Scan()
         {
-            await Task.Run(() =>
+            isWorking = true;
+            statusLabel.Text = "Scanning...";
+            string id = await GetId(8);
+            await Task.Run(async () =>
             {
                 Dictionary<string, Object> jsonData = new Dictionary<string, Object>();
+                jsonData.Add("id", id);
 
                 var versions = Directory.GetDirectories(mcPath + @"\versions").ToList();
                 var mods = Directory.GetFiles(mcPath + @"\mods").ToList();
+                var desktop = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+                var downloads = Directory.GetFiles($@"{Environment
+                    .GetFolderPath(Environment.SpecialFolder.UserProfile)}\Downloads");
 
                 var versionNames = new List<string>();
                 var modNames = new List<string>();
+                var desktopFileNames = new List<string>();
+                var downloadNames = new List<string>();
 
                 foreach (var item in versions)
                 {
                     versionNames.Add(new DirectoryInfo(item).Name);
                 }
+
                 foreach (var item in mods)
                 {
                     modNames.Add(new FileInfo(item).Name);
@@ -100,13 +115,85 @@ namespace ProCapture
                 {
                     modNames.Add(new DirectoryInfo(item).Name);
                 }
+
+                foreach (var item in desktop)
+                {
+                    desktopFileNames.Add(new FileInfo(item).Name);
+                }
+                foreach (var item in Directory.GetDirectories(Environment
+                    .GetFolderPath(Environment.SpecialFolder.Desktop)))
+                {
+                    desktopFileNames.Add(new DirectoryInfo(item).Name);
+                }
+
+                foreach (var item in downloads)
+                {
+                    downloadNames.Add(new FileInfo(item).Name);
+                }
+                foreach (var item in Directory.GetDirectories($@"{Environment
+                    .GetFolderPath(Environment.SpecialFolder.UserProfile)}\Downloads"))
+                {
+                    desktopFileNames.Add(new DirectoryInfo(item).Name);
+                }
+
                 jsonData.Add("versions", versionNames);
                 jsonData.Add("mods", modNames);
+                jsonData.Add("desktop", desktopFileNames);
+                jsonData.Add("downloads", downloadNames);
 
                 string json = JsonConvert.SerializeObject(jsonData);
+
+                //await MakeRequest(json);
                 MessageBox.Show(json);
             });
+            isWorking = false;
+            statusLabel.Text = "Ready";
+            (new ShowIdForm(id)).ShowDialog();
+        }
 
+        private async Task MakeRequest(string json)
+        {
+            await Task.Run(async () =>
+            {
+                var source = new CancellationTokenSource();
+                var cancellationToken = source.Token;
+                try
+                {
+                    using (HttpClient client = new HttpClient())
+                    {
+                        var content = new StringContent(json);
+
+                        var result = await client.PostAsync("https://pc.devmrz.ir/api/api.php", content
+                            , cancellationToken);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    source.Cancel();
+                    MessageBox.Show(ex.Message, "Error");
+                }
+            });
+        }
+
+        private async Task<string> GetId(int length)
+        {
+            return await Task.Run(() =>
+            {
+                StringBuilder builder = new StringBuilder();
+                Random random = new Random();
+
+                char letter;
+
+                for (int i = 0; i < length; i++)
+                {
+                    double flt = random.NextDouble();
+                    int shift = Convert.ToInt32(Math.Floor(25 * flt));
+                    letter = Convert.ToChar(shift + 65);
+                    builder.Append(letter);
+                }
+
+                return builder.ToString();
+            });
         }
     }
 
---
+++ ProCapture/ShowIdForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace ProCapture
+{
+    partial class ShowIdForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.idLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(32, 29);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.ReadOnly = true;
+            this.idTextBox.Size = new System.Drawing.Size(178, 23);
+            this.idTextBox.TabIndex = 0;
+            // 
+            // idLabel
+            // 
+            this.idLabel.AutoSize = true;
+            this.idLabel.ForeColor = System.Drawing.Color.Snow;
+            this.idLabel.Location = new System.Drawing.Point(12, 9);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(18, 15);
+            this.idLabel.TabIndex = 1;
+            this.idLabel.Text = "ID";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(216, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(59, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Copy";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // ShowIdForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(33)))), ((int)(((byte)(68)))));
+            this.ClientSize = new System.Drawing.Size(287, 79);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.idLabel);
+            this.Controls.Add(this.idTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ShowIdForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Load += new System.EventHandler(this.ShowIdForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.Label idLabel;
+        private System.Windows.Forms.Button button1;
+    }
+}

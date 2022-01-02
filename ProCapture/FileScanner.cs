5ed771b6d2ce5ccca0fcb0d791cc5fcac67cf2cf
xXDevSSHXx <sepehr86.HomeWork@gmail.com>
2022-01-02 15:21:57 +0330

    Added Vape Detector(Part 1)

---
+++ ProCapture/FileScanner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace ProCapture
+{
+    public static class FileScanner
+    {
+
+        public static void Initialize() => Signatures = File.ReadAllText($"{Application.StartupPath}\\Signatures.txt");
+
+        private static string GetMD5FromFile(string path)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(path))
+                {
+                    return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", string.Empty).ToLower();
+                }
+            }
+        }
+
+        public static string Signatures;
+        public static List<string> Scan(List<string> filePathes)
+        {
+            var susFiles = new List<string>();
+            foreach (var item in filePathes)
+            {
+                string file = GetMD5FromFile(item);
+
+                if (Signatures.Contains(file))
+                {
+                    susFiles.Add(item);
+                }
+            }
+            return susFiles;
+        }
+    }
+}
